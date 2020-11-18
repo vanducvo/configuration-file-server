448cf6fde6495ecc3f5bee7045a7ff94b7e7831c
Van Duc Vo <vanducvo.dev@gmail.com>
2020-11-18 15:38:54 +0700

    TDD: change class of configuration components

---
+++ components/configuration/change/index.test.js
@@ -0,0 +1,65 @@
+const Change = require('./index.js');
+
+describe('Change Class', () => {
+  const path = 'user.name';
+  const value = 10;
+
+  it('should have constructor with default arguments', () => {
+    const change = new Change(path , value);
+
+    expect(change).toBeInstanceOf(Change);
+  });
+
+  it('should have constructor with all arguments', () => {
+    const change = new Change(path, value, true);
+
+    expect(change).toBeInstanceOf(Change);
+  });
+
+  it('should have get path', () => {
+    const change = new Change(path, value);
+
+    expect(change.getPath()).toEqual(path);
+  });
+
+  it('should have set path', () => {
+    const change = new Change(path, value); 
+    const newPath = path + ".new";
+
+    change.setPath(newPath);
+
+    expect(change.getPath()).toEqual(newPath);
+  });
+
+  it('should have get value', () => {
+    const change = new Change(path, value);
+
+    expect(change.getValue()).toEqual(value);
+  });
+
+  it('should have set value', () => {
+    const change = new Change(path, value);
+    const newValue = value + 10;
+
+    change.setValue(newValue);
+
+    expect(change.getValue()).toEqual(newValue);
+  });
+
+  it('should have is delete', () => {
+    const change = new Change(path, value);
+
+    expect(change.isDelete()).toBeFalsy();
+  });
+
+  it('should have is delete', () => {
+    const change = new Change(path, value);
+
+    expect(change.isDelete()).toBeFalsy();
+
+    change.setDelete(true);
+
+    expect(change.isDelete()).toBeTruthy();
+  });
+
+});
