b29fbb01ac55aa5eead310122ec0270d0415c176
Van Duc Vo <vanducvo.dev@gmail.com>
2020-12-11 16:10:45 +0700

    custom mysql

--- pre-test.sh
+++ pre-test.sh
@@ -9,10 +9,23 @@ fi
 echo 'RUN WITH ROOT MYSQL';
 
 echo 'create user';
-mysql -uroot -e 'source ./scripts/test/create-user.sql';
+if [ -z "$1" ]
+then
+  echo 'create user';
+  mysql -uroot -e 'source ./scripts/test/create-user.sql';
+  
+  echo 'create table user of configuration_test';
+  mysql -uroot configuration_test -e 'source ./scripts/test/create-table-user.sql'
 
-echo 'create table user of configuration_test';
-mysql -uroot configuration_test -e 'source ./scripts/test/create-table-user.sql'
+  echo 'create table configuration of configuration_test';
+  mysql -uroot configuration_test -e 'source ./scripts/test/create-table-configuration.sql'
+else
+  echo 'create user';
+  mysql -uroot -p -e 'source ./scripts/test/create-user.sql';
+  
+  echo 'create table user of configuration_test';
+  mysql -uroot -p configuration_test -e 'source ./scripts/test/create-table-user.sql'
 
-echo 'create table configuration of configuration_test';
-mysql -uroot configuration_test -e 'source ./scripts/test/create-table-configuration.sql'
+  echo 'create table configuration of configuration_test';
+  mysql -uroot -p configuration_test -e 'source ./scripts/test/create-table-configuration.sql'
+fi
